3f40cd292accf8f52b35fbfe33c56a06191f0ea9
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-16 22:09:12 -0400

    Created an array list  and used a for loop to calculate the sum of the test

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -4,7 +4,22 @@ public class TestStatistics {
 
     public static void main(String[] args) {
 
-        
+        //Creating array of 10 scores
+        int[] testScores = {45, 50, 55, 65, 75, 80, 85, 90, 95, 100};
+
+        //Calculating the sum of test scores using a for loop
+        int sum = 0;
+
+        for( int i = 0; i < testScores.lenght; i++){
+            sum += testScores[i];
+
+
+        }
+
+
+
+
+
     }
 
 }
